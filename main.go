329457ab5e70e44d382701c11cfb94911f5510b3
Justin Peterson <jmp3833@rit.edu>
2016-11-12 20:10:04 -0500

    Input sanitization and help print function

--- main.go
+++ main.go
@@ -8,14 +8,32 @@ import (
 
 func main() {
 	args := os.Args[1:]
+	if len(args) < 1 {
+		printHelp()
+		return
+	}
 	switch args[0] {
 	case "chase-seed":
-		seedChaseDB(args[1])
+		handleSeedChaseInput()
 	default:
-		fmt.Println("Incorrect program args")
+		printHelp()
 	}
 }
 
 func seedChaseDB(filename string) {
 	jobs.SeedChaseDB(filename)
+}
+
+func printHelp() {
+	fmt.Println("Accepted Command line args:")
+	fmt.Println("chase-seed <path/to/csvfile> : Seed DB with CSV provided by chase bank export")
+}
+
+func handleSeedChaseInput() {
+	args := os.Args[1:]
+	if len(args) < 2 {
+		printHelp()
+		return
+	}
+	seedChaseDB(args[1])
 }
